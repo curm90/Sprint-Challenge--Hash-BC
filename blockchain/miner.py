b1c9e79069b9c7618a9f7d34b6791db21cff39c6
Liam Sutton <devliam90@gmail.com>
2020-03-13 08:07:03 +0000

    my id, proof of work

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -24,7 +24,8 @@ def proof_of_work(last_proof):
 
     print("Searching for next proof")
     proof = 0
-    #  TODO: Your code here
+    while valid_proof(last_proof, proof) is False:
+        proof -= random.random()
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
